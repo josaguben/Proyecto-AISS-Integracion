013e1cef3869a909e4807f92413f45ff4cfe064d
josaguben <josaguben@alum.us.es>
2021-05-17 18:31:25 +0200

    La clase del Reminder

--- src/main/java/aiss/model/Reminder.java
+++ src/main/java/aiss/model/Reminder.java
@@ -1,5 +1,79 @@
 package aiss.model;
 
-public class Reminder {
+import com.google.appengine.repackaged.org.joda.time.LocalDate;
 
+public class Reminder {
+	private Integer id;
+	private LocalDate fecha;
+	private String usuario;
+
+	public Reminder(Integer id, LocalDate fecha, String usuario) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.usuario = usuario;
+	}
+	public Reminder(Integer id){
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((fecha == null) ? 0 : fecha.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reminder other = (Reminder) obj;
+		if (fecha == null) {
+			if (other.fecha != null)
+				return false;
+		} else if (!fecha.equals(other.fecha))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
 }
